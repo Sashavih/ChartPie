2e536a365b8b93778c5507345dc67f43a083690c
Sasha Sasha <sashamaninaa@mail.ru>
2023-03-18 18:21:52 +0300

    feat: add component ChartPie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from "chart.js";
+import PieChart from "./components/PieChart/PieChart";
+ChartJS.register(ArcElement, Tooltip, Legend, Title);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">PieChart</h1>
+      <div className="chartBox">
+        <div >
+          <PieChart></PieChart>
+        </div>
+        <div>
+          <PieChart></PieChart>
+        </div>
+        <div >
+          <PieChart></PieChart>
+        </div>
+      </div>
     </div>
   );
 }
